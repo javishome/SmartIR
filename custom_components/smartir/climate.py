63f853f86cee125bde6035ddfa774b6bd8a8af22
anhtt <anhtt@javisco.vn>
2025-06-25 11:55:26 +0700

    fix climate 1.0

--- custom_components/smartir/climate.py
+++ custom_components/smartir/climate.py
@@ -165,19 +165,19 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
                           "place the file manually in the proper directory.")
             return
         
-        try:
-            async with aiofiles.open(device_json_path, mode='r') as j:
-                _LOGGER.debug(f"loading json file {device_json_path}")
-                content = await j.read()
-                device_data = json.loads(content)
-                _LOGGER.debug(f"{device_json_path} file loaded")
-        except Exception:
-            _LOGGER.error("The device Json file is invalid")
-            return
+    try:
+        async with aiofiles.open(device_json_path, mode='r') as j:
+            _LOGGER.debug(f"loading json file {device_json_path}")
+            content = await j.read()
+            device_data = json.loads(content)
+            _LOGGER.debug(f"{device_json_path} file loaded")
+    except Exception:
+        _LOGGER.error("The device Json file is invalid")
+        return
 
-        async_add_entities([SmartIRClimate(
-            hass, config, device_data
-        )])
+    async_add_entities([SmartIRClimate(
+        hass, config, device_data
+    )])
 
 
 class SmartIRClimate(ClimateEntity, RestoreEntity):
